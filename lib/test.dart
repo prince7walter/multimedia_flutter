4e6ec009e5e6f136a75e7b576739286d8003a956
walter <princesamuelkouame@gmail.com>
2021-03-09 18:25:09 +0000

    api search

--- lib/test.dart
+++ lib/test.dart
@@ -1,3 +1,5 @@
+/*
+
 import 'package:flutter/material.dart';
 
 class NavDrawerExample extends StatelessWidget {
@@ -69,3 +71,162 @@ class _NewPage extends MaterialPageRoute<void> {
     );
   });
 }
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class login extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LoginExample(),
+    );
+  }
+}
+
+class LoginExample extends StatefulWidget {
+  const LoginExample({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _LoginExampleState();
+
+}
+
+
+// Adapted from the text form demo in official gallery app:
+// https://github.com/flutter/flutter/blob/master/examples/flutter_gallery/lib/demo/material/text_form_field_demo.dart
+class _LoginExampleState extends State<LoginExample> {
+  final GlobalKey<FormFieldState<String>> _passwordFieldKey =
+  GlobalKey<FormFieldState<String>>();
+
+  String _name;
+  String _phoneNumber;
+  String _email;
+  String _password;
+
+  String _validateName(String value) {
+    if (value.isEmpty) return 'Name is required.';
+    final RegExp nameExp = RegExp(r'^[A-Za-z ]+$');
+    if (!nameExp.hasMatch(value)) {
+      return 'Please enter only alphabetical characters.';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    void _showSnack() => ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text(''),
+      ),
+    );
+
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+
+        children : <Widget>[
+          const SizedBox(height: 250.0),
+          // "Name" form.
+          TextFormField(
+            textCapitalization: TextCapitalization.words,
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              filled: true,
+              hintText: '',
+              labelText: 'Pseudo',
+            ),
+            onSaved: (String value) {
+              this._name = value;
+              print('name=$_name');
+            },
+            validator: _validateName,
+          ),
+          const SizedBox(height: 40.0),
+          // "Password" form.
+          PasswordField(
+            fieldKey: _passwordFieldKey,
+            helperText: '',
+            labelText: 'Mot de passe ',
+            onFieldSubmitted: (String value) {
+              setState(() {
+                this._password = value;
+              });
+            },
+          ),
+          const SizedBox(height: 24.0),
+          // "Re-type password" form.
+
+          ButtonBar(
+            alignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context,'/home'),
+                child: const Text('Connection'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PasswordField extends StatefulWidget {
+  const PasswordField({
+    this.fieldKey,
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.onSaved,
+    this.validator,
+    this.onFieldSubmitted,
+  });
+
+  final Key fieldKey;
+  final String hintText;
+  final String labelText;
+  final String helperText;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+  final ValueChanged<String> onFieldSubmitted;
+
+  @override
+  _PasswordFieldState createState() => _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      key: widget.fieldKey,
+      obscureText: _obscureText,
+      maxLength: 20,
+      onSaved: widget.onSaved,
+      validator: widget.validator,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      decoration: InputDecoration(
+        border: const UnderlineInputBorder(),
+        filled: true,
+        hintText: widget.hintText,
+        labelText: widget.labelText,
+        helperText: widget.helperText,
+        suffixIcon: GestureDetector(
+          onTap: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+          child: Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+        ),
+      ),
+    );
+  }
+}
+
+
+ */
